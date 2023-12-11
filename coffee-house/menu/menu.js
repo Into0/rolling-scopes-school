03ecb218344cd59a75c0ea1ad2b028c815da4286
Into0 <Into0@users.noreply.github.com>
2023-12-12 01:03:12 +0300

    add deploy

---
+++ coffee-house/menu/menu.js
@@ -0,0 +1,133 @@
+const response = await fetch('../assets/products.json');
+const products = await response.json();
+
+const NAV_TOGGLE = document.querySelector('#nav__toggle');
+const NAV_LINK = document.querySelectorAll('.nav__menu-link');
+const OVERLAY = document.querySelector('.overlay');
+const POPUP = document.querySelector('.popup');
+const CATEGORY = document.querySelector('.category');
+const CATEGORY_ITEM = document.querySelectorAll('.category__item');
+
+let productsId;
+
+//////////////////////////////
+
+//////////////////////////////
+
+function showMenu() {
+  if (NAV_TOGGLE.checked) {
+    document.documentElement.style.overflow = 'hidden';
+  } else {
+    document.documentElement.style.overflow = 'visible';
+  }
+}
+
+function showOverlay() {
+  OVERLAY.classList.add('overlay__show');
+  document.documentElement.style.overflow = 'hidden';
+}
+
+function hideOverlay() {
+  OVERLAY.classList.remove('overlay__show');
+  POPUP.classList.remove('popup__show');
+  NAV_TOGGLE.checked = false;
+  document.documentElement.style.overflow = 'visible';
+}
+
+function generatePopup(products) {
+  POPUP.innerHTML = `
+    <div class="popup__wrapper">
+      <div class="popup__img">
+        <img src="${products[productsId].img}" alt="popup">
+      </div>
+      <div class="popup__text">
+        <div class="popup__title">
+          <h3>${products[productsId].name}</h3>
+          <p>${products[productsId].description}</p>
+        </div>
+        <div class="popup__size">
+          <span>Size</span>
+          <div class="popup__tab tab">
+            <button class="tab__item tab__item-act btn btn_dark btn_act"><span>S</span>${products[productsId].sizes.s.size}</button>
+            <button class="tab__item btn btn_dark"><span>M</span>${products[productsId].sizes.m.size}</button>
+            <button class="tab__item btn btn_dark"><span>L</span>${products[productsId].sizes.l.size}</button>
+          </div>
+        </div>
+        <div class="popup__additives">
+          <span>Additives</span>
+          <div class="popup__tab tab">
+            <button class="tab__item btn btn_dark"><span>1</span>${products[productsId].additives[0].name}</button>
+            <button class="tab__item btn btn_dark"><span>2</span>${products[productsId].additives[1].name}</button>
+            <button class="tab__item btn btn_dark"><span>3</span>${products[productsId].additives[2].name}</button>
+          </div>
+        </div>
+        <div class="popup__total">
+          <h3>Total:</h3>
+          <h3>$${products[productsId].price}</h3>
+        </div>
+        <div class="alert">
+          <img src="../assets/icons/info-empty.svg" alt="info">
+          <p>The cost is not final. Download our mobile app to see the final price and place your order. Earn loyalty points and enjoy your favorite coffee with up to 20% discount.</p>
+        </div>
+        <button class="popup__close btn btn_dark">Close</button>
+      </div>
+    </div>`;
+  return POPUP;
+}
+
+function showPopup() {
+  if (POPUP.classList.contains('popup__show')) {
+    hideOverlay();
+  } else {
+    showOverlay();
+    POPUP.classList.add('popup__show');
+  }
+}
+
+function generateCategoryItem(products) {
+  for (let i = 0; i < CATEGORY_ITEM.length; i += 1) {
+    let item = CATEGORY_ITEM[i];
+    productsId = i;
+    item.innerHTML = `
+      <div class="category__img"><img src="${products[productsId].img}" alt="coffee 1"></div>
+      <div class="category__text">
+        <h3 class="category__title">${products[productsId].name}</h3>
+        <p class="category__p">${products[productsId].description}</p>
+        <h3 class="category__price">$${products[productsId].price}</h3>
+      </div>`;
+    }
+}
+
+function switchCategory() {
+
+}
+
+generateCategoryItem(products);
+
+//////////////////////////////
+
+//////////////////////////////
+
+CATEGORY.onclick = function(event) {
+  if (!event.target.closest('.category__item')) return;
+  productsId = event.target.closest('.category__item').dataset.id;
+  generatePopup(products);
+  showPopup();
+};
+
+POPUP.onclick = function(event) {
+  if (!event.target.closest('.popup__close')) return;
+  hideOverlay();
+};
+
+OVERLAY.addEventListener('click', hideOverlay);
+
+NAV_LINK.forEach((elem) => {
+  elem.addEventListener('click', hideOverlay);
+});
+
+NAV_TOGGLE.addEventListener('change', showMenu);
+
+window.addEventListener("resize", function(event) {
+  NAV_TOGGLE.checked = false;
+});
---
+++ coffee-house/script.js
@@ -0,0 +1,87 @@
+const NAV_TOGGLE = document.querySelector('#nav__toggle');
+const NAV_LINK = document.querySelectorAll('.nav__menu-link');
+
+const OVERLAY = document.querySelector('.overlay');
+
+const SLIDER = document.querySelector('.slider');
+const SLIDEPREV = document.querySelector('.slider__btn-prev');
+const SLIDENEXT = document.querySelector('.slider__btn-next');
+const PROGRESS = document.querySelector('.progress__item');
+const SLIDES = document.querySelector('.slider__slides');
+
+let translateX = 0;
+let progressvalue = 0;
+let interval = setInterval(moveProgress, 50);
+let slideInterval = setInterval(nextSlide, 5000);
+
+//////////////////////////////
+
+//////////////////////////////
+
+function showMenu() {
+  if (NAV_TOGGLE.checked) {
+    document.documentElement.style.overflow = 'hidden';
+  } else {
+    document.documentElement.style.overflow = 'visible';
+  }
+}
+
+function showOverlay() {
+  document.documentElement.style.overflow = 'hidden';
+}
+
+function hideOverlay() {
+  NAV_TOGGLE.checked = false;
+  document.documentElement.style.overflow = 'visible';
+}
+
+function prevSlide() {
+  SLIDES.style.transform = `translateX(${translateX += 140}%)`;
+  if (translateX > 0) {
+    translateX = 0;
+    SLIDES.style.transform = `translateX(${translateX -= 280}%)`;
+  }
+}
+
+function nextSlide() {
+  SLIDES.style.transform = `translateX(${translateX -= 140}%)`;
+  if (translateX < -280) {
+    translateX = 0;
+    SLIDES.style.transform = `translateX(${translateX -= 0}%)`;
+  }
+}
+
+function moveProgress() {
+  progressvalue = progressvalue == 100 ? 0 : progressvalue += 1;
+  PROGRESS.value = progressvalue;
+}
+
+setInterval(() => {
+  if (document.querySelector(".slider__slides:hover") != null) {
+    interval_stop();
+    interval_start();
+  }
+}, 50);
+
+function interval_start() {
+  interval = setInterval(moveProgress, 50);
+  slideInterval = setInterval(nextSlide, 5000);
+};
+
+function interval_stop() {
+  interval = clearInterval(interval);
+  slideInterval = clearInterval(slideInterval);
+};
+
+SLIDENEXT.addEventListener('click', nextSlide);
+SLIDEPREV.addEventListener('click', prevSlide);
+
+NAV_LINK.forEach((elem) => {
+  elem.addEventListener('click', hideOverlay);
+});
+
+NAV_TOGGLE.addEventListener('change', showMenu);
+
+window.addEventListener("resize", function(event) {
+  NAV_TOGGLE.checked = false;
+});
