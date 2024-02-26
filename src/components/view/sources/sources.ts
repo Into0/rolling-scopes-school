2c70801ac0d36caf4f5108ca5e9f51520981cb72
Into0 <Into0@users.noreply.github.com>
2024-02-27 01:52:37 +0300

    refactor: replace 'as'

--- src/components/view/sources/sources.ts
+++ src/components/view/sources/sources.ts
@@ -4,22 +4,29 @@ import { Source } from '../../../types/index';
 class Sources {
   draw(data: Source[]): void {
     const fragment = document.createDocumentFragment();
-    const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+    const sourceItemTemp = document.querySelector('#sourceItemTemp');
+
+    if (!sourceItemTemp || !(sourceItemTemp instanceof HTMLTemplateElement)) {
+      throw new Error('Template element not found or is not an HTMLTemplateElement');
+    }
 
     data.forEach((item): void => {
-      const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
+      const sourceClone = sourceItemTemp.content.cloneNode(true);
 
-      const sourcesItemNameElement = sourceClone.querySelector('.source__item-name') as HTMLElement;
-      sourcesItemNameElement.textContent = item.name;
+      const tempElement = document.createElement('div');
+      tempElement.appendChild(sourceClone);
 
-      const sourcesItemElement = sourceClone.querySelector('.source__item') as HTMLElement;
-      sourcesItemElement.setAttribute('data-source-id', item.id);
+      const sourcesItemNameElement = tempElement.querySelector('.source__item-name');
+      if (sourcesItemNameElement instanceof HTMLElement) sourcesItemNameElement.textContent = item.name;
 
-      fragment.append(sourceClone);
+      const sourcesItemElement = tempElement.querySelector('.source__item');
+      if (sourcesItemElement instanceof HTMLElement) sourcesItemElement.setAttribute('data-source-id', item.id);
+
+      fragment.append(tempElement);
     });
 
-    const sources = document.querySelector('.sources') as HTMLElement;
-    sources.append(fragment);
+    const sources = document.querySelector('.sources');
+    if (sources instanceof HTMLElement) sources.append(fragment);
   }
 }
 
