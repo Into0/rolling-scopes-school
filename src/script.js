e90c152db576adb3f2ed797f4c091bb12da268a6
Into0 <Into0@users.noreply.github.com>
2024-11-28 23:49:57 +0300

    feat: implement random cards for home

--- src/script.js
+++ src/script.js
@@ -8,6 +8,8 @@ const OVERLAY = document.querySelector('.overlay');
 const NAV_TOGGLE = document.querySelector('#nav__toggle');
 const NAV_LINK = document.querySelectorAll('.nav__menu-link');
 
+const GIFTS_CARDS = document.querySelector('.gifts__cards');
+
 //////////////////////////////
 
 //////////////////////////////
@@ -38,6 +40,36 @@ function hideMenu() {
   NAV_TOGGLE.checked = false;
   enableScroll();
 }
+
+function random(arr) {
+  return arr.sort((a, b) => 0.5 - Math.random());
+}
+
+function genElem(tag, name) {
+  tag = document.createElement(tag);
+  name = tag.className = `${name}`;
+  return tag;
+}
+
+function genCards(obj) {
+  for (let i = 0; i < 4; i += 1) {
+     let giftId = i;
+     let cat = obj[giftId].category.toLowerCase().split(' ');
+
+     const CARD = genElem('div', `gifts__cards-item card card-${cat[1]}`);
+     CARD.dataset.id = giftId + 1;
+     GIFTS_CARDS.append(CARD);
+
+     CARD.innerHTML = `
+      <div class="gifts__cards-img card-img card-img-${cat[1]}"></div>
+      <div class="gifts__cards-text card-text">
+        <h4 class="card--${cat[1]}">${obj[giftId].category}</h3>
+        <h3>${obj[giftId].name}</h3>
+      </div>`;
+    }
+}
+
+genCards(random(gifts));
 
 //////////////////////////////
 
