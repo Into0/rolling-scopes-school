d6e45d439205b792b2cd1929993e72736d3f31e6
Into0 <Into0@users.noreply.github.com>
2024-11-29 01:44:34 +0300

    feat: implement timer for home page

--- src/script.js
+++ src/script.js
@@ -10,6 +10,11 @@ const NAV_LINK = document.querySelectorAll('.nav__menu-link');
 
 const GIFTS_CARDS = document.querySelector('.gifts__cards');
 
+const TIMER_DAYS = document.querySelector('.timer-days');
+const TIMER_HOURS = document.querySelector('.timer-hours');
+const TIMER_MINUTES = document.querySelector('.timer-minutes');
+const TIMER_SECONDS = document.querySelector('.timer-seconds');
+
 //////////////////////////////
 
 //////////////////////////////
@@ -53,23 +58,49 @@ function genElem(tag, name) {
 
 function genCards(obj) {
   for (let i = 0; i < 4; i += 1) {
-     let giftId = i;
-     let cat = obj[giftId].category.toLowerCase().split(' ');
+    let giftId = i;
+    let cat = obj[giftId].category.toLowerCase().split(' ');
 
-     const CARD = genElem('div', `gifts__cards-item card card-${cat[1]}`);
-     CARD.dataset.id = giftId + 1;
-     GIFTS_CARDS.append(CARD);
+    const CARD = genElem('div', `gifts__cards-item card card-${cat[1]}`);
+    CARD.dataset.id = giftId + 1;
+    GIFTS_CARDS.append(CARD);
 
-     CARD.innerHTML = `
+    CARD.innerHTML = `
       <div class="gifts__cards-img card-img card-img-${cat[1]}"></div>
       <div class="gifts__cards-text card-text">
         <h4 class="card--${cat[1]}">${obj[giftId].category}</h3>
         <h3>${obj[giftId].name}</h3>
       </div>`;
-    }
+  }
 }
 
 genCards(random(gifts));
+
+function nextYearCounter() {
+  const nextYear = new Date().getFullYear() + 1;
+  const newYear = new Date(`January 1, ${nextYear} 00:00:00`);
+  const curYear = new Date();
+  const timeLeft = newYear - curYear;
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(timeLeft / day);
+  const hours = Math.floor((timeLeft % day) / hour);
+  const minutes = Math.floor((timeLeft % hour) / minute);
+  const seconds = Math.floor((timeLeft % minute) / second);
+
+  TIMER_DAYS.innerText = days;
+  TIMER_HOURS.innerText = hours;
+  TIMER_MINUTES.innerText = minutes;
+  TIMER_SECONDS.innerText = seconds;
+
+  setTimeout(nextYearCounter, 1000);
+}
+
+nextYearCounter();
 
 //////////////////////////////
 
@@ -83,7 +114,7 @@ NAV_LINK.forEach((elem) => {
   elem.addEventListener('click', hideMenu);
 });
 
-window.addEventListener('resize', function(event) {
+window.addEventListener('resize', function (event) {
   NAV_TOGGLE.checked = false;
   enableScroll();
 });
