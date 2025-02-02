bae4a97d4000cab4c7605c06d0db3a3a2f40f3c0
Into0 <Into0@users.noreply.github.com>
2025-02-03 00:14:20 +0300

    add deploy

---
+++ nonograms/script.js
@@ -0,0 +1,24 @@
+/* empty css           *//* empty css       */const response = await fetch("/nonograms.json");
+const nonograms = await response.json();
+console.log(nonograms[1].nonogram);
+function createElement(options) {
+  const { tag = "div", text = "", parent, classes = [], att = [] } = options;
+  const element = document.createElement(tag);
+  element.textContent = text;
+  if (classes.length > 0) {
+    element.classList.add(...classes);
+  }
+  if (parent != null) {
+    parent.appendChild(element);
+  }
+  if (att.length > 0) {
+    element.setAttribute(...att);
+  }
+  return element;
+}
+const mainTag = createElement({
+  tag: "main",
+  text: "",
+  classes: ["main"]
+});
+document.body.append(mainTag);
