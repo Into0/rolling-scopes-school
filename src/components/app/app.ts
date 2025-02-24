fc875686e2e2988477ab3766fc9ad217d21d89e4
Into0 <Into0@users.noreply.github.com>
2025-02-25 00:30:59 +0300

    feat: rewrite to ts app.js

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,7 +1,11 @@
+import { NewsApi } from '../../types/index';
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
+    private controller: AppController;
+    private view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
@@ -10,8 +14,8 @@ class App {
     start() {
         document
             .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+            ?.addEventListener('click', (e) => this.controller.getNews(e, (data: NewsApi) => this.view.drawNews(data)));
+        this.controller.getSources((data: NewsApi) => this.view.drawSources(data));
     }
 }
 
