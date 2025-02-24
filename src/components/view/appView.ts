816154f07341d800e096357f22ef610a08326c49
Into0 <Into0@users.noreply.github.com>
2025-02-24 19:49:19 +0300

    feat: rewrite to ts appView.js

--- src/components/view/appView.ts
+++ src/components/view/appView.ts
@@ -1,18 +1,21 @@
+import { NewsApi } from '../../types/index';
 import News from './news/news';
 import Sources from './sources/sources';
 
 export class AppView {
+    private news: News;
+    private sources: Sources;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: NewsApi) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: NewsApi) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
