757022859bd2ea661a08277c9265d623a5be0a68
Into0 <Into0@users.noreply.github.com>
2025-02-25 00:35:23 +0300

    refactor: change entry file name

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const EslingPlugin = require('eslint-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
