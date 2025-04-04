9c1e552a799162491dfdab8aafad5f836c768656
Into0 <Into0@users.noreply.github.com>
2025-04-04 21:43:04 +0300

    feat: add api requsts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,59 +1,92 @@
 class Api {
-  baseUrl = 'localhost:3000';
-  garage = `${this.baseUrl}/garage`;
-  winners = `${this.baseUrl}/winners`;
-  allCars = 0;
-  constructor () {}
+  static baseUrl = 'http://127.0.0.1:3000';
+  static garage = `${this.baseUrl}/garage`;
+  static engine = `${this.baseUrl}/engine`;
+  static winners = `${this.baseUrl}/winners`;
 
-  async getCars(page: number, limit = 7) {
+  static totalCars: number;
+  static totalWinners: number;
+
+  constructor() {}
+
+  static async getCars(page?: number, limit = 7) {
     const response = await fetch(`${this.garage}?_page=${page}&_limit=${limit}`, { method: 'GET' });
-    this.allCars = Number(response.headers.get('X-Total-count'));
-  
+    this.totalCars = Number(response.headers.get('X-Total-count'));
     return response.json();
   }
 
-  getCar() {
-    
+  static async getCar(id: number) {
+    await fetch(`${this.garage}/${id}`, { method: 'GET' });
   }
 
-  createCar() {
-
+  static async createCar(obj: object) {
+    await fetch(`${this.garage}`, {
+      method: 'POST',
+      body: JSON.stringify(obj),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
   }
 
-  deleteCar() {
-
+  static async deleteCar(id: number) {
+    await fetch(`${this.garage}/${id}`, { method: 'DELETE' });
   }
 
-  updateCar() {
-
+  static async updateCar(id: number, obj: object) {
+    await fetch(`${this.garage}/${id}`, { 
+      method: 'PUT',
+      body: JSON.stringify(obj),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
   }
 
-  startCarsEngine() {
-
+  static async startCarsEngine(id: number) {
+    await fetch(`${this.engine}?id=${id}&status=started`, { method: 'PATCH' });
   }
 
-  switchCarsEngine() {
-
+  static async stopCarsEngine(id: number) {
+    await fetch(`${this.engine}?id=${id}&status=stopped`, { method: 'PATCH' });
   }
 
-  getWinners() {
-
+  static async switchCarsEngine(id: number) {
+    await fetch(`${this.engine}?id=${id}&status=drive`, { method: 'PATCH' });
   }
 
-  getWinner() {
-
+  static async getWinners(page?: number, limit = 10) {
+    const response = await fetch(`${this.winners}?_page=${page}&_limit=${limit}`, { method: 'GET' });
+    this.totalWinners = Number(response.headers.get('X-Total-count'));
+    return response.json();
   }
 
-  createWinner() {
-
+  static async getWinner(id: number) {
+    await fetch(`${this.winners}/${id}`, { method: 'GET' });
   }
 
-  deleteWinner() {
-
+  static async createWinner(obj: object) {
+    await fetch(`${this.winners}`, {
+      method: 'POST',
+      body: JSON.stringify(obj),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
   }
 
-  updateWinner() {
+  static async deleteWinner(id: number) {
+    await fetch(`${this.winners}/${id}`, { method: 'DELETE' });
+  }
 
+  static async updateWinner(id: number, obj: object) {
+    await fetch(`${this.winners}/${id}`, { 
+      method: 'PUT',
+      body: JSON.stringify(obj),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
   }
 
 }
