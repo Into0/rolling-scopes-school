461511e5d9f1decb987e893901d29631439a4588
Into0 <Into0@users.noreply.github.com>
2025-04-05 02:47:31 +0300

    feat: implement addCar

--- src/pages/garage.ts
+++ src/pages/garage.ts
@@ -1,13 +1,46 @@
+import Api from '../api/api';
 import Page from './page';
 import Ui from './ui';
 
 class Garage extends Page {
+  private carColor = '#000000';
+  private carName = '';
   public render(): HTMLElement {
-    const pageTitle = Ui.createTitle(this.textObject.garage).getNode();
-
-    this.container.append(this.pageSwitch, pageTitle);
+    this.pageTitle.textContent = 'garage';
+    this.container.append(this.pageSwitch, this.pageTitle);
+    this.getTotalCars();
+    this.createCar();
     return this.container;
   }
+
+  private async getTotalCars(): Promise<void> {
+    await Api.getCars();
+    this.pageTitle.textContent = `garage (${Api.totalCars})`;
+  }
+
+  private createCar(): void {
+    this.container.append(
+      Ui.createCar(
+        (textInput) => {
+          this.carName = (textInput.target as HTMLInputElement).value;
+        },
+        (colorInput) => {
+          this.carColor = (colorInput.target as HTMLInputElement).value;
+        },
+        () => {
+          Api.createCar({
+            name: `${this.carName}`,
+            color: `${this.carColor}`,
+          });
+          this.getTotalCars();
+        },
+      ).getNode(),
+    );
+  }
+
+  private selectCar(): void {}
+
+  private updateCar(): void {}
 }
 
 export default Garage;
