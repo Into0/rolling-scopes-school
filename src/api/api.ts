719749cb4bb344844133ae1bcf3afbdfde6e4209
Into0 <Into0@users.noreply.github.com>
2025-04-05 02:42:38 +0300

    refactor: lint fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,94 +1,93 @@
 class Api {
-  static baseUrl = 'http://127.0.0.1:3000';
-  static garage = `${this.baseUrl}/garage`;
-  static engine = `${this.baseUrl}/engine`;
-  static winners = `${this.baseUrl}/winners`;
+  public static baseUrl = 'http://127.0.0.1:3000';
+  public static garage = `${this.baseUrl}/garage`;
+  public static engine = `${this.baseUrl}/engine`;
+  public static winners = `${this.baseUrl}/winners`;
 
-  static totalCars: number;
-  static totalWinners: number;
+  public static totalCars: number;
+  public static totalWinners: number;
 
   constructor() {}
 
-  static async getCars(page?: number, limit = 7) {
+  public static async getCars(page?: number, limit = 7): Promise<Response> {
     const response = await fetch(`${this.garage}?_page=${page}&_limit=${limit}`, { method: 'GET' });
     this.totalCars = Number(response.headers.get('X-Total-count'));
     return response.json();
   }
 
-  static async getCar(id: number) {
+  public static async getCar(id: number): Promise<void> {
     await fetch(`${this.garage}/${id}`, { method: 'GET' });
   }
 
-  static async createCar(obj: object) {
+  public static async createCar(object: object): Promise<void> {
     await fetch(`${this.garage}`, {
       method: 'POST',
-      body: JSON.stringify(obj),
+      body: JSON.stringify(object),
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
     });
   }
 
-  static async deleteCar(id: number) {
+  public static async deleteCar(id: number): Promise<void> {
     await fetch(`${this.garage}/${id}`, { method: 'DELETE' });
   }
 
-  static async updateCar(id: number, obj: object) {
-    await fetch(`${this.garage}/${id}`, { 
+  public static async updateCar(id: number, object: object): Promise<void> {
+    await fetch(`${this.garage}/${id}`, {
       method: 'PUT',
-      body: JSON.stringify(obj),
+      body: JSON.stringify(object),
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
     });
   }
 
-  static async startCarsEngine(id: number) {
+  public static async startCarsEngine(id: number): Promise<void> {
     await fetch(`${this.engine}?id=${id}&status=started`, { method: 'PATCH' });
   }
 
-  static async stopCarsEngine(id: number) {
+  public static async stopCarsEngine(id: number): Promise<void> {
     await fetch(`${this.engine}?id=${id}&status=stopped`, { method: 'PATCH' });
   }
 
-  static async switchCarsEngine(id: number) {
+  public static async switchCarsEngine(id: number): Promise<void> {
     await fetch(`${this.engine}?id=${id}&status=drive`, { method: 'PATCH' });
   }
 
-  static async getWinners(page?: number, limit = 10) {
+  public static async getWinners(page?: number, limit = 10): Promise<Response> {
     const response = await fetch(`${this.winners}?_page=${page}&_limit=${limit}`, { method: 'GET' });
     this.totalWinners = Number(response.headers.get('X-Total-count'));
     return response.json();
   }
 
-  static async getWinner(id: number) {
+  public static async getWinner(id: number): Promise<void> {
     await fetch(`${this.winners}/${id}`, { method: 'GET' });
   }
 
-  static async createWinner(obj: object) {
+  public static async createWinner(object: object): Promise<void> {
     await fetch(`${this.winners}`, {
       method: 'POST',
-      body: JSON.stringify(obj),
+      body: JSON.stringify(object),
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
     });
   }
 
-  static async deleteWinner(id: number) {
+  public static async deleteWinner(id: number): Promise<void> {
     await fetch(`${this.winners}/${id}`, { method: 'DELETE' });
   }
 
-  static async updateWinner(id: number, obj: object) {
-    await fetch(`${this.winners}/${id}`, { 
+  public static async updateWinner(id: number, object: object): Promise<void> {
+    await fetch(`${this.winners}/${id}`, {
       method: 'PUT',
-      body: JSON.stringify(obj),
+      body: JSON.stringify(object),
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
     });
   }
-
 }
 
 export default Api;
