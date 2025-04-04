3962a4f0ba8fd2ea04f5b2d28c9e4679cfd770ef
Into0 <Into0@users.noreply.github.com>
2025-04-05 02:44:28 +0300

    refactor: add ui elements

--- src/pages/ui.ts
+++ src/pages/ui.ts
@@ -1,44 +1,61 @@
-import Component from "../components/components";
-import Button from "../components/button";
+import Component from '../components/components';
+import Button from '../components/button';
+import Input from '../components/input';
 
 abstract class Ui {
-  
-  public static createMain(): Component {
-    return new Component({
-      tag: 'main',
-      className: 'main',
-    });
+  public static createTag(tag: string, className: string): Component {
+    return new Component({ tag, className });
   }
 
-  public static createPageSwitch(): Component {
+  public static createDiv(className: string): Component {
+    return new Component({ tag: 'div', className });
+  }
+
+  public static createButton(className: string, text: string, onClick?: (event: Event) => void): Component {
+    return new Button({ className, text, onClick });
+  }
+
+  public static createInput(className: string, type: string, onChange?: (event: Event) => void): Component {
+    return new Input({ className, type, onChange });
+  }
+
+  public static createSpan(className: string, text?: string): Component {
+    return new Component({ tag: 'span', className, text });
+  }
+
+  public static createTitle(className: string, text?: string): Component {
+    return new Component({ tag: 'h1', className, text });
+  }
+
+  public static createPageSwitch(
+    onClickGarage: (event: Event) => void,
+    onClickWinners: (event: Event) => void,
+  ): Component {
     return new Component(
       {
         tag: 'div',
         className: 'page-switch',
       },
-      new Button({
-        className: 'btn-garage',
-        text: 'garage',
-        onClick: (): void => {
-          globalThis.location.hash = '/garage';
-        },
-      }),
-      new Button({
-        className: 'btn-winners',
-        text: 'winners',
-        onClick: (): void => {
-          globalThis.location.hash = '/winners';
-        },
-      }),
-      );
-    }
-    public static createTitle(text?: string): Component {
-      return new Component({
-        tag: 'h1',
-        className: 'title',
-        text: text,
-      })
-    }
+      this.createButton('btn-garage', 'garage', onClickGarage),
+      this.createButton('btn-winners', 'winners', onClickWinners),
+    );
+  }
+
+  public static createCar(
+    onInput: (event: Event) => void,
+    onChangeColor: (event: Event) => void,
+    onClikCreate: (event: Event) => void,
+  ): Component {
+    return new Component(
+      {
+        tag: 'div',
+        className: 'create-car',
+      },
+      this.createInput('car-name-input', 'text', onInput),
+      this.createInput('car-color-input', 'color', onChangeColor),
+      this.createButton('btn-create', 'create', onClikCreate),
+    );
+  }
 }
 
 export default Ui;
