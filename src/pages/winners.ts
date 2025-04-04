cf644c02128d71bd17ea7c7a4e8d3e3cf2159d20
Into0 <Into0@users.noreply.github.com>
2025-04-05 02:48:06 +0300

    refactor: lint fix

--- src/pages/winners.ts
+++ src/pages/winners.ts
@@ -1,11 +1,9 @@
 import Page from './page';
-import Ui from './ui';
 
 class Winners extends Page {
   public render(): HTMLElement {
-    const pageTitle = Ui.createTitle(this.textObject.winners).getNode();
-
-    this.container.append(this.pageSwitch, pageTitle);
+    this.pageTitle.textContent = `winners`;
+    this.container.append(this.pageSwitch, this.pageTitle);
     return this.container;
   }
 }
