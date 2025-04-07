c56eb0541c86303b042731c48f49031881468db9
Into0 <Into0@users.noreply.github.com>
2025-04-08 02:51:40 +0300

    refactor: replace ui with tags

--- src/components/tags.ts
+++ src/components/tags.ts
@@ -16,16 +16,24 @@ class Tag {
     return new Button({ className, text, onClick });
   }
   
-  public static input(className: string, type: string, onChange?: (event: Event) => void): Component {
-    return new Input({ className, type, onChange });
+  public static input(className: string, type: string, value?: string, onChange?: (event: Event) => void): Component {
+    return new Input({ className, type, value, onChange });
   }
   
   public static h1(className: string, text?: string): Component {
     return new Component({ tag: 'h1', className, text });
   }
+
+  public static h2(className: string, text?: string): Component {
+    return new Component({ tag: 'h2', className, text });
+  }
   
+  public static h3(className: string, text?: string): Component {
+    return new Component({ tag: 'h3', className, text });
+  }
+
   public static h4(className: string, text?: string): Component {
-    return new Component({ tag: 'h1', className, text });
+    return new Component({ tag: 'h4', className, text });
   }
 }
 
--- src/pages/page.ts
+++ src/pages/page.ts
@@ -1,21 +1,22 @@
-import Ui from './ui';
+import Tag from '../components/tags';
 
 abstract class Page {
   protected container: HTMLElement;
   protected pageSwitch: HTMLElement;
   protected pageTitle: HTMLElement;
   constructor(id: string) {
-    this.container = Ui.createTag('main', 'main').getNode();
+    this.container = Tag.main('main').getNode();
     this.container.id = id;
-    this.pageSwitch = Ui.createPageSwitch(
-      () => {
+    this.pageSwitch = Tag.div(
+      'page-switch',
+      Tag.button('btn-garage', 'garage', () => {
         globalThis.location.hash = '/garage';
-      },
-      () => {
+      }),
+      Tag.button('btn-winners', 'winners', () => {
         globalThis.location.hash = '/winners';
-      },
-    ).getNode();
-    this.pageTitle = Ui.createTitle('title').getNode();
+      }),
+      ).getNode()
+    this.pageTitle = Tag.h1('title').getNode();
   }
 
   public render(): HTMLElement {
