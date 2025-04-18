91e000ecaf4e137593763e7c3ab439c5cb253700
Into0 <Into0@users.noreply.github.com>
2025-04-19 02:02:37 +0300

    refactor: add input component

---
+++ src/components/input.ts
@@ -0,0 +1,35 @@
+import Component from './component';
+
+interface InputOptions {
+  className?: string;
+  type: string;
+  placeholder: string;
+  onChange?: (event: Event) => void;
+  onInput?: (event: Event) => void;
+}
+
+class InputComponent extends Component {
+  private onChange?: (event: Event) => void;
+  private onInput?: (event: Event) => void;
+  constructor({ className, type, placeholder, onChange, onInput }: InputOptions) {
+    super({ tag: 'input', className });
+    this.setAttribute('type', type);
+
+    if (placeholder) {
+      this.setAttribute('placeholder', placeholder);
+    }
+    if (onChange) {
+      this.onChange = onChange;
+      this.addListener('change', this.onChange);
+    }
+    if (onInput) {
+      this.onInput = onInput;
+      this.addListener('input', this.onInput);
+    }
+  }
+  public destroy(): void {
+    super.destroy();
+  }
+}
+
+export default InputComponent;
