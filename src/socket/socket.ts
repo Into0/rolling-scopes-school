449865e6c5e4c416b880cc22c9a071eb625046b0
Into0 <Into0@users.noreply.github.com>
2025-04-19 02:09:15 +0300

    refactor: add socket

---
+++ src/socket/socket.ts
@@ -0,0 +1,78 @@
+class Socket {
+  private url = 'ws://127.0.0.1:4000';
+  private webSocket = new WebSocket(this.url);
+  private isConnected: Promise<void> | undefined;
+
+  constructor() {
+    this.init();
+  }
+
+  public init(): void {
+    this.isConnected = new Promise<void>((resolve) => {
+      this.webSocket.addEventListener('open', () => {
+        console.log('WebSocket connection open.');
+        resolve();
+      });
+
+      this.webSocket.addEventListener('message', (event) => {
+        this.handleServerResponse(event.data);
+      });
+    });
+  }
+
+  public async sendLoginRequest(id: string, login: string, password: string): Promise<void> {
+    await this.isConnected;
+    const request = {
+      id: id,
+      type: 'USER_LOGIN',
+      payload: {
+        user: {
+          login: login,
+          password: password,
+        },
+      },
+    };
+    this.webSocket.send(JSON.stringify(request));
+  }
+
+  public async sendLogoutRequest(id: string, login: string, password: string): Promise<void> {
+    await this.isConnected;
+    const request = {
+      id: id,
+      type: 'USER_LOGOUT',
+      payload: {
+        user: {
+          login: login,
+          password: password,
+        },
+      },
+    };
+    this.webSocket.send(JSON.stringify(request));
+  }
+
+  public async sendUsersRequest(id: string, type: string): Promise<void> {
+    await this.isConnected;
+    const request = {
+      id: id,
+      type: type,
+      payload: {},
+    };
+    this.webSocket.send(JSON.stringify(request));
+  }
+
+  private handleServerResponse(data: string): void {
+    const response = JSON.parse(data);
+
+    if (response.type === 'USER_ACTIVE') {
+      const users = response.payload.users;
+      console.log(users);
+    }
+
+    if (response.type === 'USER_INACTIVE') {
+      const users = response.payload.users;
+      console.log(users);
+    }
+  }
+}
+
+export default Socket;
