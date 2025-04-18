046223c4c75f9a64a0fcd68df5347287a1669a6c
Into0 <Into0@users.noreply.github.com>
2025-04-19 02:13:57 +0300

    refactor: add page file

---
+++ src/pages/page.ts
@@ -0,0 +1,15 @@
+import { main } from '../components/tags';
+
+class Page {
+  public container = main('main').getNode();
+
+  public back(): void {
+    history.back();
+  }
+
+  public render(): HTMLElement {
+    return this.container;
+  }
+}
+
+export default Page;
