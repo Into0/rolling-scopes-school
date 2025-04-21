9829f531760f32144aebf5a3ba1a0957890da3ae
Into0 <Into0@users.noreply.github.com>
2025-04-22 01:51:24 +0300

    refactor: add some route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,71 @@
+import AboutPage from './pages/about/about';
+import ChatPage from './pages/chat/chat';
 import LoginPage from './pages/login/login';
+import type Page from './pages/page';
+import Socket from './socket/socket';
 
 class App {
   private container = document.body;
+  private socket: Socket;
+
+  constructor() {
+    this.socket = new Socket();
+  }
 
   public run(): void {
-    const loginPage = new LoginPage();
-    this.container.append(loginPage.render());
+    this.locationChange();
+    this.routeChange();
+  }
+
+  private renderPage(pageId: string): void {
+    this.container.replaceChildren();
+    let page: Page | undefined = undefined;
+    const login = new LoginPage(this.socket);
+    const about = new AboutPage();
+    const chat = new ChatPage(this.socket);
+
+    switch (pageId) {
+      case '/login': {
+        page = login;
+        break;
+      }
+      case '/about': {
+        page = about;
+        break;
+      }
+      case '/chat': {
+        page = chat;
+        break;
+      }
+      default: {
+        page = login;
+        break;
+      }
+    }
+
+    if (page) {
+      this.container.append(page.render());
+    }
+  }
+
+  private routeChange(): void {
+    globalThis.addEventListener('hashchange', () => {
+      let hash = globalThis.location.hash.slice(1);
+      if (hash === '' || hash === '/') {
+        globalThis.location.hash = '/login';
+      }
+      this.renderPage(hash);
+    });
+  }
+
+  private locationChange(): this {
+    globalThis.addEventListener('load', () => {
+      if (globalThis.location.pathname.length > 0) {
+        globalThis.location.hash = '#/';
+      }
+      history.replaceState(undefined, '', `${globalThis.location.href}`);
+    });
+    return this;
   }
 }
 
